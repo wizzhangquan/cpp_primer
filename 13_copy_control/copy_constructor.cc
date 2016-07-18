52b6cd710638576cf0cafeff59e89e686de0e6f3
ZacharyWiZ <wizzhangquan@gmail.com>
2016-07-18 14:51:03 +0800

    add operator= function

--- 13_copy_control/copy_constructor.cc
+++ 13_copy_control/copy_constructor.cc
@@ -10,10 +10,15 @@ public:
     
     Point(int _x = 0, int _y = 0) : x(_x), y(_y) 
         { cout << "default constructor point x:" << x << " y:" << y << " this's addr:" << this << endl; } 
+
+    Point &operator=(const Point & rvalue) {
+        x = rvalue.x;  y = rvalue.y; 
+        cout << "operator point x:" << x << " y:" << y << " this's addr:" << this << endl;
+        return *this;
+    }
     
     void print(void) const
         { cout << "my x:" << x << " y:" << y << " this's addr:" << this << endl; }
-private:
     int x;
     int y;
 };
@@ -28,11 +33,16 @@ Point foo_bar(Point arg) {
 }
 
 int main(void) {
-    Point testp = {3, 4};
-    const Point &retp = foo_bar(testp);   /* 为何这里使用const & 就可以成功？而非const引用就不可以？ */
-    retp.print();
-    //Point retp(foo_bar(testp));           /* 为何这里在函数返回的时候调用了拷贝构造函数，但将返回值传入对象中却不调用拷贝构造函数呢 */
+    Point testp(3, 4);
+    //const Point &retp = foo_bar(testp);      /* 这里使用const & 就可以成功？而非const引用就不可以？ : 个人猜测是因为没有源对象 或者说是看不到原对象因为毕竟是返回值 */
+    //Point retp = foo_bar(testp);             /* 这里在函数返回的时候调用了拷贝构造函数，但将返回值传入对象中却不调用拷贝构造函数呢 */
+                                               /*  个人感觉是编译器的原因：因为函数返回的对象如果再次使用拷贝构造的话，返回的对象就永远不会被使用，这样浪费空间时间。
+                                                   于是就直接将其地址给了返回对象。 */
     //Point *retpp = NULL;
     //*retpp = foo_bar(testp);
+    //retp.print();
+    Point oper_p(1, 2);
+    oper_p = testp;                            /* 当不在定义处，而是已经初始化后的对象进行赋值时，会调用operator=*/
+    oper_p.print();
     return 0;
 }
