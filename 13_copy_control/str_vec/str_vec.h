ec25815067e11ae471c446184b82176d92a6def6
ZacharyWiZ <wizzhangquan@gmail.com>
2016-07-20 22:46:06 +0800

    compiler failed

--- 13_copy_control/str_vec/str_vec.h
+++ 13_copy_control/str_vec/str_vec.h
@@ -3,21 +3,33 @@
 
 #include <string>
 #include <memory>
+#include <utility>
 
-using std::string;
-using std::allocator;
+using namespace std;
+//using std::string;
+//using std::pair;
 
 class StrVec {
 public:
-    StrVec() : element(nullptr), first_free(nullptr), cap(nullptr) {}
-    StrVec(const StrVec&);
-    StrVec &operator=(const StrVec&);
-    ~StrVec();
+    StrVec() : element(NULL), first_free(NULL), cap(NULL) {}
+//    StrVec(const StrVec&);
+//    StrVec &operator=(const StrVec&);
+    ~StrVec() { free(); };
 
     void push_back(const string &);
+    size_t size() const { return first_free - element; }
+    size_t capacity() const { return cap - element; }
+    string *begin() const { return element; }
+    string *end() const { return first_free; };
 
 private:
-    static allocator<string> alloc;
+    static allocator<string>alloc;
+    void chk_n_alloc() 
+        { if (size() == capacity()) reallocate(); }
+    pair<string* , string*>
+        alloc_n_copy(const string *, const string *);
+    void free();
+    void reallocate();           //重分内存
     string *element;
     string *first_free;
     string *cap;             // end
