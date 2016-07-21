e9294ab2dc11b81e94d782bcf2bb4bfe7fde90ed
ZacharyWiZ <wizzhangquan@gmail.com>
2016-07-21 22:35:55 +0800

    just for operator

---
+++ 14_operator/sales_data/sales_data.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "sales_data.h"
+
+using namespace std;
+
+istream &
+operator>>(istream &is, SalesData &item) {
+    double price;
+    is >> item.bookNo >> item.units_sold >> price;
+    if (is) //这里检查输入是否成功
+        item.revenue = item.units_sold * price;
+    else
+        item = SalesData();
+    return is;
+}
+
+SalesData::SalesData(istream &is) : SalesData() {
+    is >> *this;
+}
+
+int main() {
+    return 0;
+}
---
+++ 14_operator/sales_data/sales_data.h
@@ -0,0 +1,28 @@
+#ifndef _SALES_DATA_H__
+#define _SALES_DATA_H__
+
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::istream;
+
+class SalesData {
+friend istream & operator>>(istream&, SalesData &item);
+
+public:
+    SalesData(const string &s, unsigned int n, double p) :
+        bookNo(s), units_sold(n), revenue(n*p) { }
+    SalesData() : SalesData("", 0, 0.0f) { }
+    SalesData(const string &s) : SalesData(s, 0, 0.0f) { }
+    SalesData(istream &);
+
+private:
+    double avg_price() const
+        { return units_sold > 0 ? revenue/units_sold : 0; }
+    string bookNo;
+    unsigned int units_sold;
+    double revenue ;
+};
+
+#endif
