afd99404c62b2ec2464e9f8837636b7b309a9da6
ZacharyWiZ <wizzhangquan@gmail.com>
2016-07-25 15:07:33 +0800

    quote

---
+++ 15_object_oriented_programming/quote/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "quote.h"
+
+using namespace std;
+
+int main() {
+    Quote q;
+    return 0;
+}
---
+++ 15_object_oriented_programming/quote/quote.h
@@ -0,0 +1,28 @@
+#ifndef _QUOTE_H__
+#define _QUOTE_H__
+
+#include <string>
+
+using std::string;
+using std::size_t;
+
+class Quote {
+public:
+    //Quote() { }; //c++98
+    Quote() = default;  //c++11
+    Quote(const string &book, double sales_price):
+        bookNo(book), price(sales_price) { }
+    
+    string isbn() const { return bookNo; }
+
+    virtual double net_price(size_t n) const
+        { return n*price; }
+    //virtual ~Quote() { }; //c++98
+    virtual ~Quote() = default; //c++11
+private:
+    string bookNo;
+protected:
+    double price ;
+};
+
+#endif
