550b09112a6a3c5627bd63762fd8a05aeb7a5db3
ZacharyWiZ <wizzhangquan@gmail.com>
2016-08-03 17:13:46 +0800

    add the auto is differ to auto &

--- design_patterns/singleton/v4_thread_static_deleteGC/singleton.cc
+++ design_patterns/singleton/v4_thread_static_deleteGC/singleton.cc
@@ -9,6 +9,19 @@ public:
         return &instance;
     }
 
+/***
+ *  static Singleton& getInstance() {
+ *      static Singleton instance;
+ *      return instance;
+ *  }
+ *  这样写也是可以的。
+ *  但是注意：在使用该返回时，不能赋予auto类型
+ *  因为会转换为Singleton来接收getInstance()返回值
+ *  那么将会调用private: Singleton(const Singleton&) = delete
+ *
+ *  auto &sobj = Singleton::getInstance(); //is right
+ * */
+
     void doSomething() {
         m_test++;
         cout << "Singleton dosomething, test:"
