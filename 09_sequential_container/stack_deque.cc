42d7fd05b4373c3ef1414c8d1c66b6cd72e1fa37
ZacharyWiZ <wizzhangquan@gmail.com>
2016-08-06 17:59:26 +0800

    test the stack<int, deque<int>>

---
+++ 09_sequential_container/stack_deque.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <deque>
+#include <stack>
+
+/*
+ * stack test
+ */
+
+using namespace std;
+
+int main() {
+    stack<int, deque<int>> stk;
+    int num;
+    cout << "how many number do u want push:";
+    cin >> num; 
+    for (int i=0; i<num; ++i)
+        stk.push(i);
+
+    cout << "the stack's top:" 
+         << stk.top() << endl;
+
+    cout << "let us pop the stk: " << endl;
+    while (!stk.empty()) {
+        cout << "pop:"
+             << stk.top() << endl;
+        stk.pop(); //pop's return is void
+    }
+    return 0;
+}
